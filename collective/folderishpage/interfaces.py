5857be5368762222f04d407dbd7665a7be084ceb
maartenkling <maarten@fourdigits.nl>
2015-05-28 15:07:01 +0200

    remove unused

--- collective/folderishpage/interfaces.py
+++ collective/folderishpage/interfaces.py
@@ -1,10 +1,5 @@
-from zope import schema
 from zope.interface import Interface
 
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
-
-from collective.folderishpage import folderishpageMessageFactory as _
 
 class IATFolderishDocument(Interface):
     """A page in the site. Can contain rich text."""
